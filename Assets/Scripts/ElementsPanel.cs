9a1aa4fa57e94fafc6c6fee0e0228b516c76cf60
Mr_Xoma <xomyakes@gmail.com>
2018-04-10 01:52:45 +0300

    Look at this!

---
+++ Assets/Scripts/ElementsPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementsPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0;i<10;i++)
+        {
+            Vector3 pos = new Vector3(this.gameObject.transform.position.x,5 - i, 0);
+            Instantiate(elements[0], pos, Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //OnMouseEnter();
+	}
+    public GameObject[] elements;
+    
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public bool explosionFlag;
+	// Use this for initialization
+	void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public GameObject explosion;
+
+    private void OnMouseDown()
+    {
+        if (explosionFlag == false)
+        {
+            Instantiate(explosion, explosion.transform.position, Quaternion.identity);
+            explosionFlag = true;
+        }
+    }
+}
