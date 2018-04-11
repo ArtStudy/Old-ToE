5bc3add20e9604dbc559a48b7f7af66557e82c94
Mr_Xoma <xomyakes@gmail.com>
2018-04-11 21:08:17 +0300

    ToE v.0.0.2

--- Assets/Scripts/StarLight.cs
+++ Assets/Scripts/StarLight.cs
@@ -16,8 +16,8 @@ public class StarLight : MonoBehaviour {
         while(true)
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane/5));
-            Instantiate(stars[Random.Range(0, stars.Length)], pos, Quaternion.identity);
-            yield return new WaitForSeconds(Random.Range(0.5f,10f));
+            Instantiate(stars[Random.Range(0, stars.Length)], pos,Quaternion.Euler(0,0,Random.Range(0,360)));
+            yield return new WaitForSeconds(Random.Range(1f,3f));
         }
     }
 }
