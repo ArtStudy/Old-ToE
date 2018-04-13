143fc14628759e577cea03c4d4a1ae45ac2ba9a4
Mr_Xoma <xomyakes@gmail.com>
2018-04-13 03:32:53 +0300

    ToE v0.0.3

--- Assets/Scripts/ElementAnimation.cs
+++ Assets/Scripts/ElementAnimation.cs
@@ -12,7 +12,7 @@ public class ElementAnimation : MonoBehaviour {
     }
     void Update()
     {
-      
+        
     }
     IEnumerator elementAnimation()
     {
--- Assets/Scripts/ElementMovement.cs
+++ Assets/Scripts/ElementMovement.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ElementMovement : MonoBehaviour {
+    private float speed = 3f;
+    private float acceleration = 3f;
     void OnMouseDrag()
     {
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1);
         Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-        transform.position = objectPos;
+        transform.position = Vector3.MoveTowards(transform.position, objectPos, speed*Time.deltaTime);
+        speed += Time.deltaTime * acceleration;
+    }
+    private void OnMouseUp()
+    {
+        speed = 3f;
+    }
+    void Update()
+    {
+        Vector3 screenBorder = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        if (transform.position.x > screenBorder.x + 0.1 || transform.position.y > screenBorder.y + 0.1 || transform.position.x < -screenBorder.x - 0.1 || transform.position.y < -screenBorder.y - 0.1)
+            Destroy(gameObject);
     }
 }
